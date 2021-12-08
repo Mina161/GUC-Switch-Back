96b41f7da50976939fe6c56735f4800a4461f768
Mina Ehab Khalil <minaehab161.me@gmail.com>
2021-12-08 18:17:56 +0200

    Multer implementation

--- app.js
+++ app.js
@@ -1,73 +1,79 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 var app = express();
-const { MongoClient } = require('mongodb');
-var bodyParser = require('body-parser');
-
+const { MongoClient } = require("mongodb");
+const multer = require('multer');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+const upload = multer();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req,res){
-  res.render("index", {title: "GUC Switch Server"})
-})
+app.get("/", function (req, res) {
+  res.render("index", { title: "GUC Switch Server" });
+});
 
-app.post("/login",async function(req, res){
-  console.log(req.body)
-  var found = await login(req.body)
-  if(found === null) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
+app.post("/login",upload.none(), async function (req, res) {
+  console.log(req.body);
+  var found = await login(req.body);
+  if (found === null) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
     res.write("User not found");
     res.end();
-  }
-  else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  } else {
+    res.writeHead(200, { "Content-Type": "json" });
     res.write(JSON.stringify(found));
     res.end();
   }
-})
+});
 
-app.post("/signup",async function(req, res){
-  console.log(req.body)
-  var err = await addUser(req.body)
-  if(err) {
+app.post("/signup",upload.none(), async function (req, res) {
+  console.log(req.body);
+  var err = await addUser(req.body);
+  if (err) {
     res.end("User Found Before");
-  }
-  else res.end("Signed in");
-})
+  } else res.end("Signed in");
+});
 
 // Client Setup
-const uri = "mongodb+srv://admin:admin@cluster0.ujdx5.mongodb.net/GUC?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri =
+  "mongodb+srv://admin:admin@cluster0.ujdx5.mongodb.net/GUC?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function addUser(data) {
   await client.connect();
-  var found = await client.db("GUC").collection("students").findOne({appNo: data.appNo})
-  if(found === null)
-    await client.db("GUC").collection("students").insertOne(data)
-  client.close()
-  return found !== null
+  var found = await client
+    .db("GUC")
+    .collection("students")
+    .findOne({ appNo: data.appNo });
+  if (found === null)
+    await client.db("GUC").collection("students").insertOne(data);
+  client.close();
+  return found !== null;
 }
 
 async function login(data) {
   await client.connect();
-  var found = await client.db("GUC").collection("students").findOne({appNo: data.appNo, password: data.password})
-  console.log(found)
-  client.close()
-  return found
+  var found = await client
+    .db("GUC")
+    .collection("students")
+    .findOne({ appNo: data.appNo, password: data.password });
+  console.log(found);
+  client.close();
+  return found;
 }
 
 async function addRequest(data) {
   await client.connect();
-  await client.db("GUC").collection("requests").insertOne(data)
-  client.close()
+  await client.db("GUC").collection("requests").insertOne(data);
+  client.close();
 }
 
 app.listen(process.env.PORT || 8080);
