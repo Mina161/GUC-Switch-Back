b75faa75acf7431782d69296ba2050a94983f160
Mina Ehab Khalil <minaehab161.me@gmail.com>
2021-12-08 17:55:49 +0200

    Body parser

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var path = require('path');
 var app = express();
 const { MongoClient } = require('mongodb');
+var bodyParser = require('body-parser');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
