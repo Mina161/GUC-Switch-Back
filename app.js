026e24d0e1d9c57572adb66b950dd8390e59369e
Mina Ehab Khalil <minaehab161.me@gmail.com>
2021-12-10 22:08:45 +0200

    ENV Variables

--- app.js
+++ app.js
@@ -127,8 +127,8 @@ var appUser = null;
 
 // Client Setup
 const uri =
-  "mongodb+srv://admin:admin" +
-  // process.env.DB_PASS +
+  "mongodb+srv://admin:" +
+  process.env.DB_PASS +
   "@cluster0.ujdx5.mongodb.net/GUC?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
