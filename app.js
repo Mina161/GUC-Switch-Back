fc311459250c1623b2caff28a203cc62fc722a1c
Mina Ehab Khalil <minaehab161.me@gmail.com>
2022-09-01 21:28:58 +0200

    Trial1

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ async function contactMatch(sender, receiver) {
 async function updateSome(data) {
   await client.connect();
   if(data.adminPass == process.env.ADMIN) {
-    var toUpdate = await client.db("GUC").collection("Students").find({...data.filters})
+    var toUpdate = await client.db("GUC").collection("Students").find(data.filters)
     toUpdate.map(person => client.db("GUC").collection("Students").findOneAndUpdate({appNo: person.appNo},{...person, password: bcrypt.hashSync(person.password, saltRounds)} ))
     console.log(toUpdate);
     return "Done";
