af45dcfa33e506a615ad84ab9f3aa338bf3c538a
Mina Ehab Khalil <minaehab161.me@gmail.com>
2022-09-01 21:34:30 +0200

    trial2

--- app.js
+++ app.js
@@ -303,8 +303,11 @@ async function updateSome(data) {
   await client.connect();
   if(data.adminPass == process.env.ADMIN) {
     var toUpdate = await client.db("GUC").collection("Students").find(data.filters)
-    toUpdate.map(person => client.db("GUC").collection("Students").findOneAndUpdate({appNo: person.appNo},{...person, password: bcrypt.hashSync(person.password, saltRounds)} ))
-    console.log(toUpdate);
+    toUpdate.map(person => {
+      console.log(person);
+      client.db("GUC").collection("Students").findOneAndUpdate({appNo: person.appNo},{...person, password: bcrypt.hashSync(person.password, saltRounds)})
+    }
+    )
     return "Done";
   } else return "Fail";
 }
