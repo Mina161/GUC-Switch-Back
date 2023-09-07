87ad624cb9b26eb2d4b8ad1718e59545c3b0b0af
Mina Khalil <mina.ehab.khalil@gmail.com>
2023-09-07 23:30:25 +0300

    use mongoose

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-var express = require("express");
-var cors = require('cors')
-var path = require("path");
+const express = require("express");
+const cors = require('cors');
+const path = require("path");
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const moment = require('moment');
 const saltRounds = parseInt(process.env.SALT);
-var app = express();
-const {
-  MongoClient
-} = require("mongodb");
+const app = express();
 const multer = require("multer");
 const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -18,17 +16,38 @@ app.set("view engine", "ejs");
 
 const upload = multer();
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors())
+app.use(cors());
 
-app.get("/", function (req, res) {
-  res.render("index", {
-    title: "GUC Switch Server"
-  });
+// Connect to MongoDB using Mongoose
+mongoose.connect("mongodb+srv://admin:admin@cluster0.ujdx5.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// Define Mongoose Schemas
+const studentSchema = new mongoose.Schema({
+  appNo: { type: String, required: true },
+  name: { type: String, required: true },
+  phoneNo: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+const Student = mongoose.model("Student", studentSchema);
+
+const requestSchema = new mongoose.Schema({
+  appNo: { type: String, required: true },
+  student: { type: mongoose.Types.ObjectId, ref: "Student" },
+  major: { type: String, enum: ["MET", "DMET", "IET", "EMS", "EDPT", "BI", "Architecture", "Applied Arts", "Civil", "Management", "Law", "Pharmacy"], required: true },
+  semester: { type: String, required: true },
+  tutNo: { type: Number, required: true },
+  goTo: [{ type: Number, required: true }],
+  germanLevel: { type: String, required: true },
+  englishLevel: { type: String, required: true },
+  contacted: [{ type: String }]
+}, { timestamps: true });
+const Request = mongoose.model("Request", requestSchema);
 
 app.post("/login", upload.none(), async function (req, res) {
   try {
@@ -50,7 +69,7 @@ app.post("/login", upload.none(), async function (req, res) {
       res.write(JSON.stringify(found));
       res.end();
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -75,7 +94,7 @@ app.post("/signup", upload.none(), async function (req, res) {
       res.write(JSON.stringify(found));
       res.end();
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -90,7 +109,7 @@ app.get("/request", upload.none(), async function (req, res) {
     });
     res.write(JSON.stringify(result));
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -105,7 +124,7 @@ app.post("/request", upload.none(), async function (req, res) {
     });
     res.write(JSON.stringify(result));
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -120,7 +139,7 @@ app.put("/request", upload.none(), async function (req, res) {
     });
     res.write(JSON.stringify(result));
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -135,7 +154,7 @@ app.delete("/request", upload.none(), async function (req, res) {
     });
     res.write("Request Deleted Successfully");
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -164,7 +183,7 @@ app.get("/match", upload.none(), async function (req, res) {
       res.write(JSON.stringify(results));
       res.end();
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -173,7 +192,7 @@ app.get("/match/contact", upload.none(), async function (req, res) {
   try {
     var response = await contactMatch(req.query.sender, req.query.receiver);
     console.log(req.query.sender, req.query.receiver)
-    if(!response) {
+    if (!response) {
       res.writeHead(200, {
         "Content-Type": "text/plain",
         "Access-Control-Allow-Origin": process.env.ORIGIN,
@@ -189,7 +208,7 @@ app.get("/match/contact", upload.none(), async function (req, res) {
       res.write("Mail Failed!");
     }
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -212,7 +231,7 @@ app.post("/generateReset", upload.none(), async function (req, res) {
     }
     res.write(response)
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
@@ -236,141 +255,130 @@ app.post("/resetPassword", upload.none(), async function (req, res) {
     }
     res.write(response);
     res.end();
-  } catch(e) {
+  } catch (e) {
     console.log(e)
   }
 });
 
-// Client Setup
-const uri = process.env.DB_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+// Modify addUser function to use Mongoose
 async function addUser(data) {
-  await client.connect();
-  var user = null;
-  var found = await client
-    .db("GUC")
-    .collection("students")
-    .findOne({
-      appNo: data.appNo
-    });
-  if (found === null) {
-    var hash = bcrypt.hashSync(data.password, saltRounds);
-    await client.db("GUC").collection("students").insertOne({
-      ...data,
-      password: hash
-    });
-    user = await client
-      .db("GUC")
-      .collection("students")
-      .findOne({
-        appNo: data.appNo
+  try {
+    const found = await Student.findOne({ appNo: data.appNo });
+
+    if (found === null) {
+      const hash = bcrypt.hashSync(data.password, saltRounds);
+      const user = new Student({
+        appNo: data.appNo,
+        name: data.name,
+        phoneNo: data.phoneNo,
+        email: data.email,
+        password: hash,
       });
+      await user.save();
+      return user;
+    }
+    return null;
+  } catch (e) {
+    console.log(e);
   }
-  return user;
 }
 
+// Modify login function to use Mongoose
 async function login(data) {
-  await client.connect();
-  var found = await client
-    .db("GUC")
-    .collection("students")
-    .findOne({
-      appNo: data.appNo
-    });
-  if (found && bcrypt.compareSync(data.password, found.password)) return found;
-  else return null
-}
+  try {
+    const found = await Student.findOne({ appNo: data.appNo });
 
-async function addRequest(data) {
-  await client.connect();
-  await client.db("GUC").collection("requests").insertOne(data);
-  return await client.db("GUC").collection("requests").findOne(data);
-}
-
-async function editRequest(data) {
-  await client.connect();
-  await client
-    .db("GUC")
-    .collection("requests")
-    .updateOne({
-      appNo: data.appNo
-    }, {
-      $set: data
-    });
-  return await client
-    .db("GUC")
-    .collection("requests")
-    .findOne({
-      appNo: data.appNo
-    });
-}
-
-async function deleteRequest(appNo) {
-  await client.connect();
-  await client.db("GUC").collection("requests").deleteOne({
-    appNo: appNo
-  });
-}
-
-async function getRequest(appNo) {
-  await client.connect();
-  let result = await client
-    .db("GUC")
-    .collection("requests")
-    .findOne({
-      appNo: appNo
-    });
-  return result;
-}
-
-async function getMatches(appNo, limit, page) {
-  await client.connect();
-
-  var myRequest = await client
-    .db("GUC")
-    .collection("requests")
-    .findOne({
-      appNo: appNo
-    });
-
-  if (myRequest === null) return "No Request Found";
-  var myArray
-  if(Array.isArray(myRequest.goTo)){
-    myArray = myRequest.goTo
-  } else {
-    myArray = [myRequest.goTo]
+    if (found && bcrypt.compareSync(data.password, found.password)) {
+      return found;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log(e);
   }
-  const query = {
-    major: myRequest.major,
-    semester: myRequest.semester,
-    tutNo: {
-      "$in" : myArray
-    },
-    germanLevel: myRequest.germanLevel,
-    englishLevel: myRequest.englishLevel,
-    goTo: myRequest.tutNo,
-  };
-
-  var allResults = await client.db("GUC").collection("requests").find(query);
-
-  var count = await allResults.count()
-
-  var results = await allResults
-    .skip(parseInt(limit) * (parseInt(page) - 1))
-    .limit(parseInt(limit))
-    .toArray();
-
-  return {
-    results: results,
-    limit: parseInt(limit),
-    thisPage: parseInt(page),
-    count: count
-  };
 }
+
+// Modify addRequest function to use Mongoose
+async function addRequest(data) {
+  try {
+    const request = new Request(data);
+    await request.save();
+    return request;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Modify editRequest function to use Mongoose
+async function editRequest(data) {
+  try {
+    const updatedRequest = await Request.findOneAndUpdate(
+      { appNo: data.appNo },
+      data,
+      { new: true }
+    );
+    return updatedRequest;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Modify deleteRequest function to use Mongoose
+async function deleteRequest(appNo) {
+  try {
+    await Request.deleteOne({ appNo: appNo });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Modify getRequest function to use Mongoose
+async function getRequest(appNo) {
+  try {
+    const result = await Request.findOne({ appNo: appNo });
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Modify getMatches function to use Mongoose
+async function getMatches(appNo, limit, page) {
+  try {
+    const myRequest = await Request.findOne({ appNo: appNo });
+
+    if (myRequest === null) {
+      return "No Request Found";
+    }
+
+    let myArray = Array.isArray(myRequest.goTo) ? myRequest.goTo : [myRequest.goTo];
+
+    const query = {
+      major: myRequest.major,
+      semester: myRequest.semester,
+      tutNo: { $in: myArray },
+      germanLevel: myRequest.germanLevel,
+      englishLevel: myRequest.englishLevel,
+      goTo: myRequest.tutNo,
+    };
+
+    const count = await Request.countDocuments(query);
+
+    const results = await Request.find(query)
+      .skip(parseInt(limit) * (parseInt(page) - 1))
+      .limit(parseInt(limit));
+
+    return {
+      results: results,
+      limit: parseInt(limit),
+      thisPage: parseInt(page),
+      count: count,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 //Mail Setup
 var transporter = nodemailer.createTransport({
@@ -381,142 +389,95 @@ var transporter = nodemailer.createTransport({
   },
 });
 
+// contactMatch function
 async function contactMatch(sender, receiver) {
-  await client.connect();
-  var sendUser = await client
-    .db("GUC")
-    .collection("students")
-    .findOne({
-      appNo: sender
-    });
+  try {
+    const sendUser = await Student.findOne({ appNo: sender });
+    const recUser = await Student.findOne({ appNo: receiver }, { name: 1, email: 1 });
 
-  var recUser = await client
-    .db("GUC")
-    .collection("students")
-    .findOne({
-      appNo: receiver
-    }, {
-      projection: {
-        name: 1,
-        email: 1
-      }
-    });
-  
-  console.log(sendUser && recUser)
-  if(sendUser!==null && recUser!==null) {
-      await client
-      .db("GUC")
-      .collection("requests")
-      .updateOne({
-        appNo: sender
-      }, {
-        $push: {
-          contacted: receiver
-        }
-      });
+    if (sendUser !== null && recUser !== null) {
+      await Request.updateOne({ appNo: sender }, { $push: { contacted: receiver } });
 
-      var mailOptions = {
+      const mailOptions = {
         from: process.env.EMAIL,
         to: recUser.email,
         subject: "Switching Partner Found!",
-        html: "<h1>Hello " +
-        recUser.name +
-          "</h1><p>We found you a switching partner</p><br /><p>Name: " +
-          sendUser.name +
-          ", Mobile Number: " +
-          sendUser.phoneNo +
-          ", email: " +
-          sendUser.email +
-          "</p><br /><p>Give them a call to confirm the switch</p>",
+        html: `<h1>Hello ${recUser.name}</h1><p>We found you a switching partner</p><br /><p>Name: ${sendUser.name}, Mobile Number: ${sendUser.phoneNo}, email: ${sendUser.email}</p><br /><p>Give them a call to confirm the switch</p>`,
       };
 
       await transporter.sendMail(mailOptions);
-  } else {
-    return "Mail Failed"
-  }
-  
-}
-
-// Forgot Password Functions
-async function generatePasswordReset(data) {
-  await client.connect();
-  var user = await client.db("GUC").collection("students").findOne({appNo: data.appNo,
-    email: data.email})
-  var alreadyRequested = user?.token 
-  if(!alreadyRequested){
-    var ttl = moment().add(10,'minutes').toDate();
-    var token = crypto.randomBytes(32).toString("hex");
-    var updated = await client.db("GUC").collection("students").updateOne({
-      appNo: data.appNo,
-      email: data.email
-    }, {
-      $set: {
-        token: token,
-        TTL: ttl
-      }
-    })
-    if (user){
-      var mailOptions = {
-        from: process.env.EMAIL,
-        to: user?.email,
-        subject: "Password Reset Request",
-        html: "<h1>Hello " + user?.appNo + "!</h1>" +
-          "<p>Have you requested to reset your password? Follow this link and reset your password within 10 minutes</p><br/>" +
-          "<a href=\"" + process.env.BASE + "reset-password/" + token + "\">Click Here</a><br/>" +
-          "<p>If that doesn't work follow this link: "+process.env.BASE+"reset-password/" + token +"</p><br/"+
-          "<p>Not you? Ignore this email and secure your password</p>"
-      };
-      await transporter.sendMail(mailOptions); 
-      return "Mail Sent!"
-    } else 
-      return "User-Email not found"
-  }
-  return "Already sent a mail or user not found"
-}
-
-async function resetPassword(data) {
-  await client.connect();
-  var toReset = await client.db("GUC").collection("students").findOne({
-    appNo: data.appNo,
-    email: data.email,
-    token: data.token
-  })
-  if (toReset != null) {
-    expiry = moment(toReset.TTL)
-    today = moment()
-    if (expiry > today) {
-      var hash = bcrypt.hashSync(data.password, saltRounds);
-      client.db("GUC").collection("students").updateOne({
-        appNo: data.appNo,
-        email: data.email,
-        token: data.token
-      }, {
-        $unset: {
-          token: "",
-          TTL: ""
-        },
-        $set: {
-          password: hash
-        }
-      })
-      return "Reset Done"
     } else {
-      client.db("GUC").collection("students").updateOne({
-        appNo: data.appNo,
-        email: data.email,
-        token: data.token
-      }, {
-        $unset: {
-          token: "",
-          TTL: ""
-        }
-      })
-      return "Token Expired"
+      return "Mail Failed";
     }
-  } else {
-    return "Malformed Token"
+  } catch (e) {
+    console.log(e);
   }
 }
+
+// generatePasswordReset function
+async function generatePasswordReset(data) {
+  try {
+    const user = await Student.findOne({ appNo: data.appNo, email: data.email });
+    const alreadyRequested = user?.token;
+
+    if (!alreadyRequested) {
+      const ttl = moment().add(10, 'minutes').toDate();
+      const token = crypto.randomBytes(32).toString("hex");
+      await Student.updateOne({ appNo: data.appNo, email: data.email }, { $set: { token: token, TTL: ttl } });
+
+      if (user) {
+        const mailOptions = {
+          from: process.env.EMAIL,
+          to: user?.email,
+          subject: "Password Reset Request",
+          html: `<h1>Hello ${user?.appNo}!</h1><p>Have you requested to reset your password? Follow this link and reset your password within 10 minutes</p><br/><a href="${process.env.BASE}reset-password/${token}">Click Here</a><br/><p>If that doesn't work follow this link: ${process.env.BASE}reset-password/${token}</p><br/><p>Not you? Ignore this email and secure your password</p>`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        return "Mail Sent!";
+      } else {
+        return "User-Email not found";
+      }
+    }
+    return "Already sent an email or user not found";
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// resetPassword function
+async function resetPassword(data) {
+  try {
+    const toReset = await Student.findOne({ appNo: data.appNo, email: data.email, token: data.token });
+
+    if (toReset != null) {
+      const expiry = moment(toReset.TTL);
+      const today = moment();
+
+      if (expiry > today) {
+        const hash = bcrypt.hashSync(data.password, saltRounds);
+        await Student.updateOne(
+          { appNo: data.appNo, email: data.email, token: data.token },
+          { $unset: { token: "", TTL: "" }, $set: { password: hash } }
+        );
+
+        return "Reset Done";
+      } else {
+        await Student.updateOne(
+          { appNo: data.appNo, email: data.email, token: data.token },
+          { $unset: { token: "", TTL: "" } }
+        );
+
+        return "Token Expired";
+      }
+    } else {
+      return "Malformed Token";
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 app.listen(process.env.PORT || 8080);
 
