376640e4fc9b452310dfaffbe5af9dc593906a97
Mina Khalil <mina.ehab.khalil@gmail.com>
2023-09-07 23:46:34 +0300

    require dotenv

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const multer = require("multer");
 const nodemailer = require("nodemailer");
 const mongoose = require("mongoose");
 
+require('dotenv').config()
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
